b7670407fa25def393d50dd7b27bd45a982bd336
GuillaumeCisco <guillaumecisco@gmail.com>
2017-04-12 15:56:57 +0200

    Fix deploy script

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,5 +1,5 @@
 #! /usr/bin/env node
-import shell from 'shelljs';
+var shell = require("shelljs");
 
 shell.exec("aws s3 --region eu-central-1 cp ./build/frontend s3://dreem-fronts/analytics/ --recursive");
 shell.exec("aws cloudfront create-invalidation --cli-input-json '{\"DistributionId\":\"E3BTH1EYFN3UC7\",\"InvalidationBatch\":{\"Paths\":{\"Quantity\":1,\"Items\":[\"/analytics/*\"]},\"CallerReference\":\"'\"$(uuidgen)\"'\"}}'");
