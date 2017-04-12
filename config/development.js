c89ef40fe864e2d63913aa2687cb1a37d8d4b76b
GuillaumeCisco <guillaumecisco@gmail.com>
2017-04-12 11:08:56 +0200

    Update favicon

--- config/development.js
+++ config/development.js
@@ -1,4 +1,4 @@
-//const apiUrl = 'http://localhost:8001/v1/dreem/ghost'; // eslint-disable-line no-var
+//const apiUrl = 'http://localhost:8001';
 const apiUrl = 'https://bender-api.rythm.co';
 const storageApiUrl = 'http://localhost:8000';
 const orchestratorApiUrl = 'http://localhost:5000';
