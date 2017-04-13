384dc6f1f888b76ff767db8beac527359dbbfc65
GuillaumeCisco <guillaumecisco@gmail.com>
2017-04-13 11:10:15 +0200

    Fix chinese default text in general modal

--- src/frontend/js/presentation/modals/error.js
+++ src/frontend/js/presentation/modals/error.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Modal} from 'antd';
+import {Modal, Button} from 'antd';
 import onlyUpdateForKeys from 'recompose/onlyUpdateForKeys';
 
 const style = {
@@ -8,19 +8,31 @@ const style = {
     display: 'block',
 };
 
-const ErrorModal = ({isVisible, error, onClose}) =>
-    <Modal
+const getButtons = (onClick) => [
+    <Button key="ok" type="primary" onClick={onClick}>
+        Ok
+    </Button>
+];
+
+const ErrorModal = ({isVisible, error, onClose}) => {
+
+    // TODO memoize function
+    const Buttons = getButtons(onClose);
+
+    return (<Modal
         title="Server Error 5xx"
         visible={isVisible}
         onOk={onClose}
         onCancel={onClose}
+        footer={Buttons}
     >
         <h1>An error occured from the server</h1>
         <p>Please report this issue to Etienne or Guillaume.
             Sometimes the server struggles to serve our data.
             Please retry in a few minutes</p>
         {error && <span className="error" style={style}>{error}</span>}
-    </Modal>;
+    </Modal>);
+};
 
 ErrorModal.propTypes = {
     isVisible: PropTypes.bool.isRequired,
