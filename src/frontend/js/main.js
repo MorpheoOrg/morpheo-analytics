3d1a918e225529ef76b29236d45acf9b39d96339
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2017-04-24 16:14:46 +0200

    Clean notebook app.

--- src/frontend/js/main.js
+++ src/frontend/js/main.js
@@ -1,38 +1,61 @@
 /* globals document */
 
-import {createStore, applyMiddleware} from 'redux';
+import 'babel-core/register';
+import 'babel-polyfill';
+
+import FastClick from 'fastclick';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {createRenderer} from 'fela';
+import {Provider as FelaProvider} from 'react-fela';
+import {AsyncComponentProvider} from 'react-async-component';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import {createStore, applyMiddleware} from 'redux';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
-// import { createLogger } from 'redux-logger'
+
+import ReactHotLoader from './ReactHotLoader';
 import App from './components/App';
 import reducer from './reducers';
 import {addCell, connectKernel} from './actions';
 
 
+const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inpsb3RlbiJ9.6kZ-0Y96-gAzrOXzqH91F9WAgAAFXpRaayVifYjuEv4';
 const middleware = [thunk];
 // if (process.env.NODE_ENV !== 'production') {
 //   middleware.push(createLogger());
 // }
 
-const jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Inpsb3RlbiJ9.6kZ-0Y96-gAzrOXzqH91F9WAgAAFXpRaayVifYjuEv4';
-
-
 const store = createStore(
   reducer,
   applyMiddleware(...middleware),
-  // pesisted_state
 );
 
 store.dispatch(addCell());
 store.dispatch(connectKernel('127.0.0.1', '8080', jwt));
 
-console.log(store.getState());
+FastClick.attach(document.body);
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'),
-);
+const root = document.getElementById('root');
+const renderer = createRenderer();
+const mountNode = document.getElementById('stylesheet');
+
+const renderApp = (RootElement) => {
+    const app = (<ReactHotLoader key={Math.random()}>
+        <AsyncComponentProvider>
+            <FelaProvider renderer={renderer} mountNode={mountNode}>
+                {/* Put that into Root*/}
+                <Provider store={store}>
+                    <RootElement />
+                </Provider>
+            </FelaProvider>
+        </AsyncComponentProvider>
+    </ReactHotLoader>);
+
+    render(app, root);
+};
+
+renderApp(App);
