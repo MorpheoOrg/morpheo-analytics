47f5b42397803846957a43f3efeae3dc14b29a47
GuillaumeCisco <guillaumecisco@gmail.com>
2017-06-07 10:14:38 +0200

    Add Caddy support

--- tools/deploy.js
+++ tools/deploy.js
@@ -1,7 +1,11 @@
 #! /usr/bin/env node
 const shell = require('shelljs');
 
-// shell.exec("aws s3 --region eu-central-1 cp ./build/frontend s3://dreem-fronts/analytics/ --recursive");
-// shell.exec("aws cloudfront create-invalidation --cli-input-json '{\"DistributionId\":\"E3BTH1EYFN3UC7\",\"InvalidationBatch\":{\"Paths\":{\"Quantity\":1,\"Items\":[\"/analytics/*\"]},\"CallerReference\":\"'\"$(uuidgen)\"'\"}}'");
+// shell.exec("aws s3 --region eu-central-1 cp ./build/frontend s3://dreem-fronts/notebook/ --recursive");
+// shell.exec("aws cloudfront create-invalidation --cli-input-json '{\"DistributionId\":\"E3BTH1EYFN3UC7\",\"InvalidationBatch\":{\"Paths\":{\"Quantity\":1,\"Items\":[\"/notebook/*\"]},\"CallerReference\":\"'\"$(uuidgen)\"'\"}}'");
 
-shell.exec('npm run build && docker build -t registry.morpheo.io/notebook . && docker push registry.morpheo.io/notebook');
+const timestamp = new Date().getTime();
+
+console.log(`Deploying registry.morpheo.io/notebook:${timestamp}`);
+
+shell.exec(`docker build -t registry.morpheo.io/notebook:${timestamp} . && docker push registry.morpheo.io/notebook:${timestamp} && kubectl --namespace staging set image deployment/notebook notebook=registry.morpheo.io/notebook:${timestamp}`);
