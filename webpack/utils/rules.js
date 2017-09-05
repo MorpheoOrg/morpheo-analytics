b611e296e7b64df35f4342ea3fa6529c5baf195f
GuillaumeCisco <guillaumecisco@gmail.com>
2017-09-05 12:25:39 +0200

    Fix package tooling

--- webpack/utils/rules.js
+++ webpack/utils/rules.js
@@ -9,8 +9,8 @@ export default env => [
     {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        //use: 'happypack/loader?id=babel',
-        use: 'babel-loader',
+        use: 'happypack/loader?id=babel',
+        //use: 'babel-loader',
     }, {
         test: /\.jpe?g$|\.gif$|\.png$/,
         use: 'url-loader?limit=10000&name=/[hash].[ext]',
