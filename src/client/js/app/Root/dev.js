cf6d2b46423a933578f0b7a1d3499bb9e795ff48
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2017-11-03 17:04:03 +0100

    Change ReactMarkdown link to open into a browser

--- src/client/js/app/Root/dev.js
+++ src/client/js/app/Root/dev.js
@@ -2,12 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {Provider} from 'react-redux';
 
+import DevTools from '../../../../common/DevTools';
 import Routes from '../../../../common/routes';
 
 const Root = ({store}) =>
     (<Provider store={store}>
         <div>
             <Routes/>
+            {/* {<DevTools/>} */}
         </div>
     </Provider>);
 
--- src/client/js/business/models/problem/components/detail/ProblemContent.js
+++ src/client/js/business/models/problem/components/detail/ProblemContent.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import styled, {css} from 'react-emotion';
 import ReactMarkdown from 'react-markdown';
+import {shell} from 'electron';
 
 import description from './description.md';
 
@@ -12,6 +13,29 @@ import description from './description.md';
 // const ProblemContent = styled(ReactMarkdown)`
 //     overflow: auto;
 // `;
+
+class RouterLink extends React.Component {
+    handleOnClick = () => {
+        shell.openExternal(this.props.href);
+    }
+
+    style = css`
+        color: -webkit-link;
+        text-decoration: underline;
+        cursor: pointer;
+    `
+
+    render() {
+        console.log(this.props);
+        return (<a
+            css={this.style}
+            onClick={this.handleOnClick}
+        >
+            {this.props.children}
+        </a>);
+    }
+};
+
 
 class ProblemContent extends React.Component {
     style = css`
@@ -25,7 +49,10 @@ class ProblemContent extends React.Component {
         return (<div
             css={this.style}
         >
-            <ReactMarkdown source={description} />
+            <ReactMarkdown
+                source={description}
+                renderers={{Link: RouterLink}}
+            />
         </div>);
     }
 }
