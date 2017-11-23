1a49228b481c47e5a23d2338a83f6ef364641c38
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2017-11-23 11:26:53 +0100

    Clean storage code and add Editor selectors.

--- src/client/js/business/models/storage_problem/actions.js
+++ src/client/js/business/models/storage_problem/actions.js
@@ -32,7 +32,11 @@
  * The fact that you are presently reading this means that you have had
  * knowledge of the CeCILL license and that you accept its terms.
  */
-import {actions as itemActions, actionTypes as itemActionTypes} from '../../../actions/item';
+import {
+    actions as itemActions,
+    actionTypes as itemActionTypes
+} from '../../../actions/item';
+
 
 const prefix = 'MODELS::STORAGE_PROBLEM';
 
