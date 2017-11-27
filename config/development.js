8fe72953a6135b9421f43be3097a5b2c9b3c1588
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2017-11-27 16:23:30 +0100

    Remove the use of devsecret file.

--- config/development.js
+++ config/development.js
@@ -1,11 +1,3 @@
-// uncomment this for testing with prod data
-import {
-    NOTEBOOK_SERVICES_USER, NOTEBOOK_SERVICES_PASSWORD,
-    ORCHESTRATOR_USER, ORCHESTRATOR_PASSWORD,
-    STORAGE_USER, STORAGE_PASSWORD,
-} from './devsecret';
-
-
 const servicesApiUrl = 'http://127.0.0.1:8000';
 
 module.exports = {
@@ -13,13 +5,5 @@ module.exports = {
         frontend: {
             services_api_url: servicesApiUrl,
         },
-    },
-    credentials: {
-        NOTEBOOK_SERVICES_USER,
-        NOTEBOOK_SERVICES_PASSWORD,
-        ORCHESTRATOR_USER,
-        ORCHESTRATOR_PASSWORD,
-        STORAGE_USER,
-        STORAGE_PASSWORD,
     },
 };
