1cb726d50a9424dd54f9661ce28dec40940c95ec
Thierry Guillemot <thierry.guillemot.work@gmail.com>
2017-12-04 15:03:09 +0100

    Change name of release files.

--- build_tools/travis/before_deploy.sh
+++ build_tools/travis/before_deploy.sh
@@ -3,17 +3,6 @@ if [ $TRAVIS_PULL_REQUEST = "false" ]; then
     # Clean the release folder by letting
     find release -mindepth 1 ! \( -name '*.deb' -o -name '*.dmg' \) -exec rm -rf {} +
 
-    ls release
-
-    if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-        # Electron builder create file of the form analytics-X.X.X.dmg on mac
-        # we rename into analytics_X.X.X_mac.dmg
-        for file in release/*; do
-            filename=${file%%.dmg};
-            mv $file ${filename/-/_}_mac.dmg;
-        done
-    fi
-
     # Create file analytics_latest_*.dmg
     for file in release/*; do
         cp ${file} ${file/_[0-9]*_/_latest_};
